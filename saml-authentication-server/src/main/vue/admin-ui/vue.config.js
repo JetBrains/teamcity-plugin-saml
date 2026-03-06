840a7292539b2a53e41797ce6a772138fc686bca
nastasia.chubatova <nastasia.chubatova@jetbrains.com>
2026-03-06 08:35:28 +0100

    Collect third-party licenses

--- saml-authentication-server/src/main/vue/admin-ui/vue.config.js
+++ saml-authentication-server/src/main/vue/admin-ui/vue.config.js
@@ -1,8 +1,24 @@
+const LicenseChecker = require('@jetbrains/ring-ui-license-checker');
+
+function createLicenseChecker(filename) {
+    return new LicenseChecker({
+        format: require('./third-party-licenses-json'),
+        filename,
+        exclude: [/@jetbrains/],
+        surviveLicenseErrors: true,
+    });
+}
+
 module.exports = {
     indexPath: "index.html",
     publicPath: "./",
     pluginOptions: {
 
+    },
+    configureWebpack: {
+        plugins: [
+            createLicenseChecker('./js-related-libraries.json'),
+        ],
     },
     filenameHashing: false,
     chainWebpack(config) {
